68c4c95430cc08b82e1b4eb2583a16097b273fc5
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-24 12:48:47 -0700

    [M1] [Backend] Add error catching in util func

--- backend/utils.js
+++ backend/utils.js
@@ -1,8 +1,13 @@
 const bcrypt = require("bcryptjs");
 
 const hashPassword = async (plaintextPassword) => {
-    const hashedPassword = await bcrypt.hash(plaintextPassword, 12);
-    return hashedPassword;
+    try {
+        const hashedPassword = await bcrypt.hash(plaintextPassword, 12);
+        return hashedPassword;
+    } catch (err) {
+        console.error("Hashing failed (bcryptjs):", err);
+        throw err;
+    }
 };
 
 module.exports = { hashPassword };
