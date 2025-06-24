9ff2cc122eb3437077be5da26477f9cece8b8bf2
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-24 10:31:17 -0700

    [M0] [Frontend] Set navigate path on logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import "../styles/Header.css"
+import { useNavigate } from "react-router";
 
 const Header = () => {
+    let navigate = useNavigate();
     return (
-        <header className="header-container">
+        <header className="header-container" onClick={() => {navigate("/")}}>
             <div className="logo-container">
                 <img src="/frog.svg" alt="Origami Logo" className="img-container"></img>
                 <h1>Origami</h1>
