5773370d3ecaea9803312a2b439e113f96c48322
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-24 20:23:16 -0700

    [M1] [Frontend] Add User page to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,22 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
+import UserAuthPage from "./components/UserAuthPage";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
     return (
         <>
-            <Header/>
+            <Header />
             <Routes>
-                <Route path="/" element={<div>Root Page</div>}/>
+                <Route path="/" element={<div>Root Page</div>} />
+                <Route element={<UserAuthPage />}>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Register />} />
+                </Route>
             </Routes>
-            <Footer/>
+            <Footer />
         </>
     );
 }
