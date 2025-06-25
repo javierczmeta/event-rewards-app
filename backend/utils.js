acf7fce491ccc766a232e9b0e5534bb776875952
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-25 10:21:56 -0700

    [M1] [Backend] Add verify function. Change bcrypt to argon

--- backend/utils.js
+++ backend/utils.js
@@ -1,13 +1,22 @@
-const bcrypt = require("bcryptjs");
+const argon2 = require("argon2");
 
 const hashPassword = async (plaintextPassword) => {
     try {
-        const hashedPassword = await bcrypt.hash(plaintextPassword, 12);
+        const hashedPassword = await argon2.hash(plaintextPassword, 12);
         return hashedPassword;
     } catch (err) {
-        console.error("Hashing failed (bcryptjs):", err);
+        console.error("Hashing failed (argon):", err);
         throw err;
     }
 };
 
-module.exports = { hashPassword };
+async function verifyPassword(plainPassword, hash) {
+    try {
+        return await argon2.verify(hash, plainPassword);
+    } catch (err) {
+        console.error("Hashing failed (argon):", err);
+        throw err;
+    }
+}
+
+module.exports = { hashPassword, verifyPassword };
