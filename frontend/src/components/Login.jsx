5204489a90006fb9580481d374a23d71fded1c60
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-24 20:51:45 -0700

    [M1] [Frontend] Add form state to components

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,12 +1,21 @@
+import { useFormInput } from "../utils/useFormInput";
+
 const Login = () => {
+    const usernameProps = useFormInput("");
+    const passProps = useFormInput("");
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <form className="auth-form">
+        <form className="auth-form" onSubmit={handleLoginSubmit}>
             <h2>Welcome Back!</h2>
             <div className="login-fields">
-                <input type="text" placeholder="Username"></input>
-                <input type="password" placeholder="Password"></input>
+                <input type="text" placeholder="Username" {...usernameProps} required></input>
+                <input type="password" placeholder="Password" {...passProps} required></input>
             </div>
-            <button className="action-button">Log In</button>
+            <button className="action-button" type="submit">Log In</button>
         </form>
     );
 };
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,16 +1,54 @@
+import { useFormInput } from "../utils/useFormInput";
+
 const Register = () => {
+    const nameInput = useFormInput("");
+    const usenameInput = useFormInput("");
+    const passInput = useFormInput("");
+    const repeatInput = useFormInput("");
+    const dobInput = useFormInput("");
+
+
+    const handleSignUpSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <form className="auth-form">
+        <form className="auth-form" onSubmit={handleSignUpSubmit}>
             <h2>Create an Account</h2>
-            <input type="text" placeholder="Display Name"></input>
-            <input type="text" placeholder="Username"></input>
-            <input type="password" placeholder="Password"></input>
-            <input type="password" placeholder="Repeat Password"></input>
+            <input
+                type="text"
+                placeholder="Display Name"
+                {...nameInput}
+                required
+            ></input>
+            <input
+                type="text"
+                placeholder="Username"
+                {...usenameInput}
+                required
+                minLength={3}
+                maxLength={30}
+            ></input>
+            <input
+                type="password"
+                placeholder="Password"
+                {...passInput}
+                required
+                minLength={8}
+            ></input>
+            <input
+                type="password"
+                placeholder="Repeat Password"
+                {...repeatInput}
+                required
+                pattern={`${passInput.value}`}
+                title="Match password"
+            ></input>
             <label>Date of Birth:</label>
-            <input type="date"></input>
-            <button className="action-button">Sign Up</button>
+            <input type="date" {...dobInput} required></input>
+            <button className="action-button" type="sumbit">Sign Up</button>
         </form>
-    )
-}
+    );
+};
 
 export default Register;
