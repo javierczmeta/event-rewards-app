7bb2b2406737d382b20a02d7d34a5b102409bb8c
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-26 12:55:40 -0700

    [M1] [Frontend] Change query provider location and add the user context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,20 @@
 import ReactDOM from "react-dom/client";
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import { UserProvider } from "./contexts/UserContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <UserProvider>
+                <App />
+            </UserProvider>
+        </QueryClientProvider>
+    </BrowserRouter>
 );
