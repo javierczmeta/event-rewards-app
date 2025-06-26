45de67b56fabccddb819ee320f2328476572ba0e
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-26 12:54:34 -0700

    [M1] [Frontend] Create context to store user data

---
+++ frontend/src/contexts/UserContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState, useContext, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    const { status, data, error, refetch } = useQuery({
+        queryKey: ["user"],
+        queryFn: () => {
+            const url = import.meta.env.VITE_SERVER_API;
+            return axios.get(`${url}/me`, {
+                withCredentials: true,
+            });
+        },
+        refetchOnWindowFocus: false
+    });
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        if (status === "success") {
+            setUser(data.data);
+        } else if (status === "error") {
+            setUser(null);
+        }
+    }, [status]);
+
+
+    return (
+        <UserContext.Provider value={{ user, refetch }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export const useUser = () => useContext(UserContext);
