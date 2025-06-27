17194d5e1c3a613438bb4b3cfe6641561a27d844
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-27 13:54:43 -0700

    [M2] [Frontend] Add image to example

--- frontend/src/components/EventFeed.jsx
+++ frontend/src/components/EventFeed.jsx
@@ -13,6 +13,7 @@ const EventFeed = () => {
         created_at: "2025-06-27T00:00:00.000Z",
         price: "Free",
         rewards: 1000,
+        image: "https://media.istockphoto.com/id/610560418/photo/facebook-menlo-park-campus-headquarters.jpg?s=612x612&w=0&k=20&c=Dqq5Dk8w4Gw1YUq5zKm0aeutMiP6A6tSPSeF1u4GCAA=",
         description:
             "The first event in the famous menlo park. Come celebrate the first week of my project",
         tags: ["Celebration", "Javier", "Joy"],
