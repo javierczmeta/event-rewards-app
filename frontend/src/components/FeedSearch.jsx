23109abf16d06d92a83ba385bc16fcc4b0f3a071
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-27 16:52:04 -0700

    [M2] [Frontend] Create SearchBar component, pass input state

--- frontend/src/components/FeedSearch.jsx
+++ frontend/src/components/FeedSearch.jsx
@@ -1,13 +1,11 @@
 import "../styles/FeedSearch.css";
-import { Search, SlidersHorizontal, Funnel } from "lucide-react";
+import {SlidersHorizontal, Funnel } from "lucide-react";
+import SearchBar from "./SearchBar";
 
-const FeedSearch = () => {
+const FeedSearch = ({searchFieldProps}) => {
     return (
         <div className="feed-options-container">
-            <div className="feed-tool-container">
-                Search Bar{" "}
-                <Search className="feed-search-icon" strokeWidth={3} size={25} color="white"/>
-            </div>
+            <SearchBar searchFieldProps={searchFieldProps}/>
             <div className="feed-tool-container"><SlidersHorizontal size={20}/> Filters</div>
             <div className="feed-tool-container"><Funnel size={20}/> Sort</div>
         </div>
