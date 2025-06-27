6f6d11d568e433820abd7296b230f0dfabacce37
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-27 15:55:45 -0700

    [M2] [Frontend] Add geocoding to Event component to get location name

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -1,35 +1,49 @@
 import "../styles/Event.css";
+import { useQuery } from "@tanstack/react-query";
+import axios from 'axios'
 
 const Event = ({ event }) => {
     const date = new Date(event.start_time).toLocaleString();
 
+    const getEventLocation = useQuery({
+        queryKey: [event.id],
+        queryFn: () => {
+            const apiToken = import.meta.env.VITE_GEOCODING_TOKEN;
+            const url = `https://api.mapbox.com/search/geocode/v6/reverse?longitude=${event.longitude}&latitude=${event.latitude}&access_token=${apiToken}`;
+            return axios.get(url);
+        },
+        refetchOnWindowFocus: false,
+    });
+
     return (
         <div className="event-card">
-
             <div className="event-image">
-                {event.image ? <img src={event.image} alt={event.name}></img> : <></>}
+                {event.image ? (
+                    <img src={event.image} alt={event.name}></img>
+                ) : (
+                    <></>
+                )}
             </div>
             <div className="event-info">
                 <h2>{event.name}</h2>
                 <p>{date}</p>
                 <p>
-                    Placeholder location {event.latitude} {event.longitude}
-                    {/* Will use geocoder api to get location */}
+                    {getEventLocation.isError && `Error Fetching Location`}
+                    {getEventLocation.isPending && "Fetching Location"}
+                    {getEventLocation.isSuccess && getEventLocation.data.data.features[0].properties.context.place.name}
                 </p>
                 <p>{event.price}</p>
                 <p>{event.rewards} point(s)</p>
                 <div className="tag-container">
                     {event.tags
-                        ? event.tags.map((tag) => (
-                                <div className="tag">{tag}</div>
+                        ? event.tags.map((tag, index) => (
+                                <div key={index} className="tag">{tag}</div>
                             ))
                         : "No tags"}
                 </div>
             </div>
             <div className="event-description">
-                <p>
-                    {event.description}
-                </p>
+                <p>{event.description}</p>
             </div>
         </div>
     );
