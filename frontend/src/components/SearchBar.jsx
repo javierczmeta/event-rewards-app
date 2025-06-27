75dbae4a1a46ce2d183a546c1a6f17a384f86d43
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-27 16:52:04 -0700

    [M2] [Frontend] Create SearchBar component, pass input state

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,33 @@
+import { Search } from "lucide-react";
+import { useState } from "react";
+
+const SearchBar = ({searchFieldProps}) => {
+    const [expanded, setExpanded] = useState(false);
+    const searchClass = "feed-tool-container" + (expanded ? " expanded" : "");
+
+    return (
+        <div
+            className={searchClass}
+            onClick={() => {
+                setExpanded(!expanded);
+            }}
+        >
+            {expanded ? (
+                <form>
+                    <input type="text" className="search-input" autoFocus placeholder="Search Bar" {...searchFieldProps}></input>
+                </form>
+            ) : (
+                <p>Search Bar</p>
+            )}
+
+            <Search
+                className="feed-search-icon"
+                strokeWidth={3}
+                size={25}
+                color="white"
+            />
+        </div>
+    );
+};
+
+export default SearchBar;
