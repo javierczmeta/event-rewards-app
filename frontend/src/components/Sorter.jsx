8880447540166ae76deb925f45980f94dce8d203
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-30 11:49:26 -0700

    [M2] [Frontend] Create UI for sorting

--- frontend/src/components/Sorter.jsx
+++ frontend/src/components/Sorter.jsx
@@ -1,12 +1,44 @@
 import "../styles/Sorter.css";
 import { Funnel } from "lucide-react";
+import useComponentVisible from "../utils/useComponentVisible";
+import { useState } from "react";
+import FilterOption from "./FilterOption";
 
 const Sorter = () => {
-    return (
-        <div className="feed-tool-container">
-            <Funnel size={20} /> Sort
+    const {
+        ref,
+        isComponentVisible: showSorters,
+        setIsComponentVisible: setShowSorters,
+    } = useComponentVisible(false);
+    const [sortState, setSortState] = useState(null);
+
+    const sortOptions = [
+        "Name",
+        "Event Start Date",
+        "Event Posting Date",
+        "Points",
+    ];
+
+    const containerClass = "feed-tool-container " + (sortState ? "sort-selected" : "")
+
+    if (!showSorters) {
+        return (
+            <div
+                className={containerClass}
+                onClick={() => {
+                    setShowSorters(true);
+                }}
+            >
+                <Funnel size={20} /> {sortState ? "Sorting by " + sortState : "Sort"} 
+            </div>
+        );
+    } else {
+        return <div ref={ref} className="sorters-container">
+            {sortOptions.map((option, index) => (
+                <FilterOption option={option} key={index} setShowSorters={setShowSorters} sortState={sortState} setSortState={setSortState}/>
+            ))}
         </div>
-    );
+    }
 };
 
 export default Sorter;
