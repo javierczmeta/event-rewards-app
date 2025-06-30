0881cde0ad1d1ff904d131703001c531e6e1ae52
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-30 11:27:47 -0700

    [M2] [Frontend] Create sorter component

--- frontend/src/components/FeedSearch.jsx
+++ frontend/src/components/FeedSearch.jsx
@@ -1,13 +1,16 @@
 import "../styles/FeedSearch.css";
-import {SlidersHorizontal, Funnel } from "lucide-react";
+import { SlidersHorizontal } from "lucide-react";
 import SearchBar from "./SearchBar";
+import Sorter from "./Sorter";
 
-const FeedSearch = ({searchFieldProps}) => {
+const FeedSearch = ({ searchFieldProps }) => {
     return (
         <div className="feed-options-container">
-            <SearchBar searchFieldProps={searchFieldProps}/>
-            <div className="feed-tool-container"><SlidersHorizontal size={20}/> Filters</div>
-            <div className="feed-tool-container"><Funnel size={20}/> Sort</div>
+            <SearchBar searchFieldProps={searchFieldProps} />
+            <div className="feed-tool-container">
+                <SlidersHorizontal size={20} /> Filters
+            </div>
+            <Sorter />
         </div>
     );
 };
---
+++ frontend/src/components/Sorter.jsx
@@ -0,0 +1,12 @@
+import "../styles/Sorter.css";
+import { Funnel } from "lucide-react";
+
+const Sorter = () => {
+    return (
+        <div className="feed-tool-container">
+            <Funnel size={20} /> Sort
+        </div>
+    );
+};
+
+export default Sorter;
