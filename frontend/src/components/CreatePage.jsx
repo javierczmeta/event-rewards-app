c04ff4e305f6b6859fc14f35d386d256098e285f
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-02 16:16:25 -0700

    [M3] [Frontend] Add form states

--- frontend/src/components/CreatePage.jsx
+++ frontend/src/components/CreatePage.jsx
@@ -3,32 +3,67 @@ import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { ToastContainer, toast, Slide } from "react-toastify";
 import CreateMap from "./CreateMap";
-import "../styles/CreatePage.css"
+import "../styles/CreatePage.css";
 
 const CreatePage = () => {
-    const longitudeProps = useFormInput(-122.1486120978705)
-    const latitudeProps = useFormInput(37.4845092388847)
+    const formInputs = {
+        longitudeProps: useFormInput(-122.1486120978705),
+        latitudeProps: useFormInput(37.4845092388847),
+        nameProps: useFormInput(""),
+        imageProps: useFormInput(""),
+        startDateProps: useFormInput(""),
+        endDateProps: useFormInput(""),
+        priceProps: useFormInput(),
+        catProps: useFormInput("Miscellaneous"),
+    };
 
     return (
         <main className="create-main">
-            
-            
-                <h2>Create a new event</h2>
-                <CreateMap longitudeProps={longitudeProps} latitudeProps={latitudeProps}/>
-                <p>Longitude: {longitudeProps.value}</p>
-                <p>Latitude: {latitudeProps.value}</p>
-                <form className="create-form">
-                <div>
-                    <input type="text" placeholder="Name" required></input>
-                    <input type="text" placeholder="Image Link"></input>
-                    <label>Start Time</label>
-                    <input type="date" required></input>
-                    <label>End Time</label>
-                    <input type="date" required></input>
-                    <input type="number" placeholder="Price" required></input>
-                    <select required>
+            <h2>Create a new event</h2>
+            <CreateMap formInputs={formInputs} />
+            <p>Longitude: {formInputs.longitudeProps.value}</p>
+            <p>Latitude: {formInputs.latitudeProps.value}</p>
+            <form className="create-form">
+                <div className="form-components-container">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        required
+                        {...formInputs.nameProps}
+                    ></input>
+
+                    <input
+                        type="text"
+                        placeholder="Image Link"
+                        {...formInputs.imageProps}
+                    ></input>
+
+                    <div>
+                        <label>Start Time: </label>
+                        <input
+                            type="datetime-local"
+                            required
+                            {...formInputs.startDateProps}
+                        ></input>
+                    </div>
+
+                    <div>
+                        <label>End Time: </label>
+                        <input
+                            type="datetime-local"
+                            required
+                            {...formInputs.endDateProps}
+                        ></input>
+                    </div>
+
+                    <div>
+                        <label>Price in $</label>
+                        <input type="number" placeholder="Price" required {...formInputs.priceProps}></input>
+                    </div>
+
+                    <select required {...formInputs.catProps}>
                         <option value="Miscellaneous">
-                            --Please choose an option--
+                            --Category: Please choose an option--
                         </option>
                         <option value="Music and Arts">Music and Arts</option>
                         <option value="Sports and Fitness">
