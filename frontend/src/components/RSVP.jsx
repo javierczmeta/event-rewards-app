ffbe25d27f45c23784b6baa534d1001072c86055
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-03 16:46:39 -0700

    [M7] [Frontend] Link Frontend with routes

--- frontend/src/components/RSVP.jsx
+++ frontend/src/components/RSVP.jsx
@@ -4,39 +4,113 @@ import axios from "axios";
 import { useUser } from "../contexts/UserContext";
 import { useParams } from "react-router";
 import { useState } from "react";
+import { ToastContainer, toast, Slide } from "react-toastify";
+
 
 const RSVP = () => {
     const { user } = useUser();
+    const { eventID } = useParams();
+    const queryClient = useQueryClient()
 
-    const [rsvpState, setRsvpState] = useState(null);
+    const getRSVP = useQuery({
+        queryKey: ["rsvp", user.id, eventID],
+        queryFn: () => {
+            const url = import.meta.env.VITE_SERVER_API;
+            return axios.get(`${url}/events/${eventID}/rsvp`, {
+                withCredentials: true,
+            });
+        },
+        refetchOnWindowFocus: false,
+        refetchInterval: 1000 * 60 * 5,
+        staleTime: 0,
+    });
+
+    const setRSVPMutation = useMutation({
+        mutationFn: (status) => {
+            if (!getRSVP.isSuccess) {throw new Error("Did not fetch data.")}
+            const url = import.meta.env.VITE_SERVER_API;
+            
+            console.log(getRSVP.data.data)
+            if (getRSVP.data.data.length === 0) {
+                return axios.post(`${url}/events/${eventID}/rsvp`, {status} , {
+                withCredentials: true,
+            });
+            } else {
+                return axios.patch(`${url}/events/${eventID}/rsvp`, {status} , {
+                withCredentials: true,
+            });
+            }
+            
+        },
+        onSuccess: () => {
+            toast.success("Success");
+            queryClient.invalidateQueries("rsvp");
+        },
+        onError: (e) => {
+            console.log(e);
+            if (e.response) {
+                toast.error(e.response.data.message);
+            } else {
+                toast.error("Unknown Error... Try again later");
+            }
+        },
+    })
+
+    if (getRSVP.isPending) {
+        return <p>Loading...</p>;
+    }
+
+    if (getRSVP.isError) {
+        return <p>Error...</p>;
+    }
+
+    let rsvpState = getRSVP.data.data;
+    if (rsvpState != []) {
+        rsvpState = rsvpState.status
+    }
 
     return (
-        <div className="rsvp-container">
-            <button
-                className={rsvpState === "Going" ? "active" : ""}
-                onClick={() => {
-                    setRsvpState("Going");
-                }}
-            >
-                Going!
-            </button>
-            <button
-                className={rsvpState === "Maybe" ? "active" : ""}
-                onClick={() => {
-                    setRsvpState("Maybe");
-                }}
-            >
-                Maybe
-            </button>
-            <button
-                className={rsvpState === "Not Going" ? "active" : ""}
-                onClick={() => {
-                    setRsvpState("Not Going");
-                }}
-            >
-                Not Going...
-            </button>
-        </div>
+        <>
+            <div className="rsvp-container">
+                <button
+                    className={rsvpState === "Going" ? "active" : ""}
+                    onClick={() => {
+                        setRSVPMutation.mutate("Going");
+                    }}
+                >
+                    Going!
+                </button>
+                <button
+                    className={rsvpState === "Maybe" ? "active" : ""}
+                    onClick={() => {
+                        setRSVPMutation.mutate("Maybe");
+                    }}
+                >
+                    Maybe
+                </button>
+                <button
+                    className={rsvpState === "Not Going" ? "active" : ""}
+                    onClick={() => {
+                        setRSVPMutation.mutate("Not Going");
+                    }}
+                >
+                    Not Going...
+                </button>
+            </div>
+            <ToastContainer
+                    position="top-right"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick={false}
+                    rtl={false}
+                    pauseOnFocusLoss={false}
+                    draggable={false}
+                    pauseOnHover
+                    theme="light"
+                    transition={Slide}
+                />
+        </>
     );
 };
 
