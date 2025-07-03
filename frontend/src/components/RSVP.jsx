4efa525ac9f6a22de003519441e95f94d9bf1b7c
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-03 15:56:09 -0700

    [M7] [Frontend] Add primitive states to rsvp

--- frontend/src/components/RSVP.jsx
+++ frontend/src/components/RSVP.jsx
@@ -3,15 +3,39 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useUser } from "../contexts/UserContext";
 import { useParams } from "react-router";
+import { useState } from "react";
 
 const RSVP = () => {
-    const {user} = useUser();
+    const { user } = useUser();
+
+    const [rsvpState, setRsvpState] = useState(null);
 
     return (
         <div className="rsvp-container">
-            <button>Going!</button>
-            <button>Maybe</button>
-            <button>Not Going</button>
+            <button
+                className={rsvpState === "Going" ? "active" : ""}
+                onClick={() => {
+                    setRsvpState("Going");
+                }}
+            >
+                Going!
+            </button>
+            <button
+                className={rsvpState === "Maybe" ? "active" : ""}
+                onClick={() => {
+                    setRsvpState("Maybe");
+                }}
+            >
+                Maybe
+            </button>
+            <button
+                className={rsvpState === "Not Going" ? "active" : ""}
+                onClick={() => {
+                    setRsvpState("Not Going");
+                }}
+            >
+                Not Going...
+            </button>
         </div>
     );
 };
