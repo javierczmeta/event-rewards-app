91696d7b0b292f7b8a6707ac8c215b042f41bad0
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-09 12:17:17 -0700

    [M5] Create Marker and show info on hover

--- frontend/src/components/Marker.jsx
+++ frontend/src/components/Marker.jsx
@@ -1,22 +1,42 @@
 import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
+import { createPortal } from "react-dom";
+import "../styles/Marker.css";
+import EventImage from "./EventImage"
+import { createDateWithOffset } from "../utils/createDateWithOffset";
+import { useNavigate } from "react-router";
 
 const Marker = ({ map, event }) => {
-
     const markerRef = useRef();
+    const contentRef = useRef(document.createElement("div"));
+    const date = createDateWithOffset(event.start_time)
+    const navigate = useNavigate()
 
     useEffect(() => {
-        markerRef.current = new mapboxgl.Marker()
+        markerRef.current = new mapboxgl.Marker(contentRef.current)
             .setLngLat([event.longitude, event.latitude])
             .addTo(map)
-            .setPopup(new mapboxgl.Popup().setHTML(`<h3>${event.name}</h3>`));
 
         return () => {
             markerRef.current.remove();
         };
     }, []);
 
-    return null;
+    return (
+        <>
+            {createPortal(
+                <div className="map-marker" onClick={() => {
+                navigate(`/feed/${event.id}`);
+            }}>
+                    <EventImage image={event.image} alt={event.name} className="map-marker-image"/>
+                    <h4 className="more-info">{event.name}</h4>
+                    <p className="more-info">{date.toDateString()}</p>
+                    <p className="more-info">{date.toLocaleTimeString()}</p>
+                </div>,
+                contentRef.current
+            )}
+        </>
+    );
 };
 
 export default Marker;
