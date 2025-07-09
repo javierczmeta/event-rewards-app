a608f9048792c64b169b0808612a414908827307
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-09 12:16:45 -0700

    [M5] Make Map Card fly to location instead of redirecting

--- frontend/src/components/MapEventCard.jsx
+++ frontend/src/components/MapEventCard.jsx
@@ -1,7 +1,6 @@
 import EventImage from "./EventImage";
 import "../styles/MapEventCard.css";
 import { useReverseGeocoding } from "../utils/useReverseGeocoding";
-import { useNavigate } from "react-router";
 
 const MapEventCard = ({ event, mapRef }) => {
     const getEventLocation = useReverseGeocoding(
@@ -10,16 +9,21 @@ const MapEventCard = ({ event, mapRef }) => {
         event.latitude
     );
 
-    const navigate = useNavigate();
-
     return (
-        <div className="map-event-card" onClick={() => {
-                navigate(`/feed/${event.id}`);
-            }}>
+        <div
+            className="map-event-card"
+            onClick={() => {
+                mapRef.current.flyTo({
+                    center: [event.longitude, event.latitude],
+                    zoom: 20,
+                });
+            }}
+        >
             <EventImage image={event.image} className="map-event-image" />
             <h2>{event.name}</h2>
             <p className="map-event-address">
-                {getEventLocation.isSuccess && getEventLocation.data.data.features[0] &&
+                {getEventLocation.isSuccess &&
+                    getEventLocation.data.data.features[0] &&
                     getEventLocation.data.data.features[0].properties
                         .place_formatted}
             </p>
