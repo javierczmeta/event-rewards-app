c7746e60c9240898609e32d9df356a3f234790f2
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-08 22:44:26 -0700

    [M7] [Backend] Merge rsvp routes to one

--- backend/test/rsvp.test.js
+++ backend/test/rsvp.test.js
@@ -7,11 +7,11 @@ jest.mock("../generated/prisma", () => {
         rSVP: {
             create: jest.fn(),
             findMany: jest.fn(),
-            update: jest.fn()
+            update: jest.fn(),
         },
         event: {
-            findUnique: jest.fn()
-        }
+            findUnique: jest.fn(),
+        },
     };
     return { PrismaClient: jest.fn(() => mPrismaClient) };
 });
@@ -31,6 +31,7 @@ describe("POST /events/:id/rsvp", () => {
     beforeEach(async () => {
         agent = request.agent(server);
         await agent.get("/set"); // Simulate setting session
+        jest.resetAllMocks()
     });
 
     it("should return 400 if validation fails", async () => {
@@ -49,16 +50,6 @@ describe("POST /events/:id/rsvp", () => {
             "ID of the event has to be an integer"
         );
     });
-    it("should return 409 if RSVP already exists", async () => {
-        prisma.rSVP.findMany.mockResolvedValueOnce([{ id: 1 }]);
-        const response = await agent
-            .post("/events/1/rsvp")
-            .send({ status: "Going" });
-        expect(response.status).toBe(409);
-        expect(response.body.message).toContain(
-            "RSVP data for this user and event already exixts"
-        );
-    });
     it("should return 404 if event does not exist", async () => {
         prisma.rSVP.findMany.mockResolvedValueOnce([]);
         prisma.event.findUnique.mockResolvedValueOnce(null);
@@ -70,10 +61,10 @@ describe("POST /events/:id/rsvp", () => {
             "The event with the specified ID does not exist"
         );
     });
-    it("should create a new RSVP and return it", async () => {
-        prisma.rSVP.findMany.mockResolvedValueOnce([]);
+    it("should update the RSVP and return it", async () => {
+        prisma.rSVP.findMany.mockResolvedValueOnce([{ id: 1 }]);
         prisma.event.findUnique.mockResolvedValueOnce({ id: 1 });
-        prisma.rSVP.create.mockResolvedValueOnce({
+        prisma.rSVP.update.mockResolvedValueOnce({
             id: 1,
             user_id: 1,
             event_id: 1,
@@ -90,55 +81,17 @@ describe("POST /events/:id/rsvp", () => {
             status: "going",
         });
     });
-});
-
-
-describe("PATCH /events/:id/rsvp", () => {
-    let agent;
-    beforeEach(async () => {
-        agent = request.agent(server);
-        await agent.get("/set"); // Simulate setting session
-    });
-
-    it("should return 400 if validation fails", async () => {
-        const response = await agent
-            .patch("/events/1/rsvp")
-            .send({ invalidField: "invalidValue" });
-        expect(response.status).toBe(400);
-        expect(response.body.message).toBeDefined();
-    });
-
-    it("should return 400 if event ID is not an integer", async () => {
-        const response = await agent
-            .patch("/events/notAnInteger/rsvp")
-            .send({ status: "Going" });
-        expect(response.status).toBe(400);
-        expect(response.body.message).toContain(
-            "ID of the event has to be an integer"
-        );
-    });
-
-    it("should return 404 if RSVP does not exist", async () => {
+    it("should create a new RSVP and return it", async () => {
         prisma.rSVP.findMany.mockResolvedValueOnce([]);
-        const response = await agent
-            .patch("/events/1/rsvp")
-            .send({ status: "Going" });
-        expect(response.status).toBe(404);
-        expect(response.body.message).toContain(
-            "RSVP data for this user and event does not exixt"
-        );
-    });
-
-    it("should update the RSVP and return it", async () => {
-        prisma.rSVP.findMany.mockResolvedValueOnce([{ id: 1 }]);
-        prisma.rSVP.update.mockResolvedValueOnce({
+        prisma.event.findUnique.mockResolvedValueOnce({ id: 1 });
+        prisma.rSVP.create.mockResolvedValueOnce({
             id: 1,
             user_id: 1,
             event_id: 1,
             status: "going",
         });
         const response = await agent
-            .patch("/events/1/rsvp")
+            .post("/events/1/rsvp")
             .send({ status: "Going" });
         expect(response.status).toBe(200);
         expect(response.body).toEqual({
