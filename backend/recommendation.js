2e459d700ecb500a6f9e3d1468047ce64852853c
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-09 15:38:55 -0700

    [TC1] [Backend] Create probability by similarity function

--- backend/recommendation.js
+++ backend/recommendation.js
@@ -58,4 +58,67 @@ async function calculateCategoryWeights(userId) {
     return weights
 }
 
-module.exports = { calculateCategoryWeights }
+/**
+ * Calculates the similarity between user1 and user2
+ * @param {number} user1_id 
+ * @param {number} user2_id 
+ * @returns float between -1 and 1
+ */
+async function similarity(user1_id, user2_id) {
+    // Get Sets
+    let G1 = await prisma.rSVP.findMany({where: {user_id: user1_id, status: "Going"}, select: {event_id: true}})
+    let G2 = await prisma.rSVP.findMany({where: {user_id: user2_id, status: "Going"}, select: {event_id: true}})
+    let N1 = await prisma.rSVP.findMany({where: {user_id: user1_id, status: "Not Going"}, select: {event_id: true}})
+    let N2 = await prisma.rSVP.findMany({where: {user_id: user2_id, status: "Not Going"}, select: {event_id: true}})
+
+    // Only care about event ids
+    G1 = G1.map(x => x.event_id)
+    G2 = G2.map(x => x.event_id)
+    N1 = N1.map(x => x.event_id)
+    N2 = N2.map(x => x.event_id)
+
+    // Convert to set
+    G1 = new Set(G1)
+    G2 = new Set(G2)
+    N1 = new Set(N1)
+    N2 = new Set(N2)
+
+    // Jacard Index
+    const numerator = G1.intersection(G2).size + N1.intersection(N2).size - N1.intersection(G2).size - G1.intersection(N2).size
+    const denominator = G1.union(G2).union(N1).union(N2).size
+
+    if (denominator === 0) {return 0}
+    return numerator / denominator
+}
+
+/**
+ * Calculates probability user is going to a certain event by calculating similarity with attendees
+ * @param {number} userId 
+ * @param {number} eventId 
+ * @returns float -1 to 1
+ */
+async function probabilityGoing(userId, eventId) {
+    let E_G = await prisma.rSVP.findMany({where: {event_id: eventId, status: "Going"}, select: {user_id: true}})
+    let E_N = await prisma.rSVP.findMany({where: {event_id: eventId, status: "Not Going"}, select: {user_id: true}})
+
+    let Z_G = 0
+    for (const user of E_G) {
+        const otherId = user.user_id
+        Z_G += await similarity(userId, otherId)
+    }
+
+    let Z_N = 0
+    for (const user of E_N) {
+        const otherId = user.user_id
+        Z_N += await similarity(userId, otherId)
+    }
+
+    const numerator = Z_G - Z_N
+    const denominator = E_G.length + E_N.length
+
+    if (denominator === 0) {return 0}
+
+    return numerator / denominator
+}
+
+module.exports = { calculateCategoryWeights, probabilityGoing }
