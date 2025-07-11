9cf9ff57d059df0176e66dcd800907a93ec53d46
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-11 12:08:03 -0700

    [M4] [Backend] Modify routes that update rsvp to include uptade time

--- backend/server.js
+++ backend/server.js
@@ -465,6 +465,7 @@ server.post(
         user_id: sessionID,
         event_id: req.params.eventId,
         status,
+        updated_at: new Date(Date.now()),
     };
 
     // Already one
@@ -548,7 +549,7 @@ server.patch(
         // Update it
         const updateRSVP = await prisma.rSVP.update({
             where: {id: req.rsvp.id},
-            data: {status: "Going", check_in_time: new Date(Date.now())}
+            data: {status: "Going", check_in_time: new Date(Date.now()), updated_at: new Date(Date.now()),}
         });
 
         const updatedEvent = await updatePoints(prisma, req.params.eventId); 
