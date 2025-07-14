e847065bd8ab16f8038d44cced4c9950a75feabf
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-14 16:25:59 -0700

    [M6] [Frontend] Add option to badge to only show icons, add onclick prop

--- frontend/src/components/Badge.jsx
+++ frontend/src/components/Badge.jsx
@@ -1,10 +1,12 @@
 import '../styles/Badge.css'
 import {DynamicIcon} from 'lucide-react/dynamic'
 
-const Badge = ({badge, className}) => {
-    return (<div className={className}>
+const Badge = ({badge, className, onClick, onlyIcons}) => {
+    onlyIcons = onlyIcons === true
+    className = `${className} ${badge.color}`
+    return (<div className={className} onClick={onClick}>
         <DynamicIcon name={badge.icon} className='badge-icon'/>
-        <p className='badge-info'>{badge.name}</p>
+        {!onlyIcons && <p className='badge-info'>{badge.name}</p>}
     </div>)
 }
 
