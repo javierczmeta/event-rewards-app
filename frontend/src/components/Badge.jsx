36342a1c4bd157445dbf4490781c8cf78a2b4e71
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-14 16:55:19 -0700

    [M6] [Frontend] Create hover state

--- frontend/src/components/Badge.jsx
+++ frontend/src/components/Badge.jsx
@@ -1,13 +1,47 @@
-import '../styles/Badge.css'
-import {DynamicIcon} from 'lucide-react/dynamic'
+import "../styles/Badge.css";
+import { DynamicIcon } from "lucide-react/dynamic";
+import { useState } from "react";
 
-const Badge = ({badge, className, onClick, onlyIcons}) => {
-    onlyIcons = onlyIcons === true
-    className = `${className} ${badge.color}`
-    return (<div className={className} onClick={onClick}>
-        <DynamicIcon name={badge.icon} className='badge-icon'/>
-        {!onlyIcons && <p className='badge-info'>{badge.name}</p>}
-    </div>)
-}
+const Badge = ({ badge, className, onClick, onlyIcons, mousePosition }) => {
+    const [hovered, setHovered] = useState(false);
+
+    onlyIcons = onlyIcons === true;
+    className = `${className} ${badge.color}`;
+
+    return (
+        <>
+            <div
+                className={className}
+                onClick={onClick}
+                onMouseEnter={() => {
+                    setHovered(true);
+                }}
+                onMouseLeave={() => {
+                    setHovered(false);
+                }}
+            >
+                <DynamicIcon name={badge.icon} className="badge-icon" />
+                {!onlyIcons && <p className="badge-info">{badge.name}</p>}
+            </div>
+
+            {mousePosition && hovered ? (
+                <div
+                    className={`hover-badge ${badge.color}`}
+                    style={{
+                        top: mousePosition.y,
+                        left: mousePosition.x,
+                    }}
+                >
+                    <DynamicIcon name={badge.icon} className="badge-icon" size={40}/>
+                    <h4>{badge.name}</h4>
+                    <p>{badge.description}</p>
+
+                </div>
+            ) : (
+                <></>
+            )}
+        </>
+    );
+};
 
 export default Badge;
