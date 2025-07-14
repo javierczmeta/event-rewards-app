372ebbf5573b4f928086c6caee7afb5a959e48f7
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-14 16:27:14 -0700

    [M6] [Frontend] Create Badge Selector component

---
+++ frontend/src/components/BadgeSelector.jsx
@@ -0,0 +1,98 @@
+import Badge from "./Badge";
+import "../styles/BadgeSelector.css";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+
+const BadgeSelector = ({ badges, setIsEditing, display_badges }) => {
+    const queryClient = useQueryClient();
+
+    const [shown, setShown] = useState(display_badges);
+
+    const toggleBadge = (badge) => {
+        const isShown = shown.find((x) => x.id === badge.id) !== undefined;
+        // Add to list
+        if (!isShown && shown.length < 3) {
+            setShown((prev) => prev.concat(badge));
+        }
+        // Remove
+        if (isShown) {
+            setShown((prev) => prev.filter((x) => x.id !== badge.id));
+        }
+    };
+
+    const badgeMutation = useMutation({
+        mutationFn: (displayBadges) => {
+            const url = import.meta.env.VITE_SERVER_API;
+            return axios.patch(`${url}/badges`, {badges: displayBadges}, {
+                withCredentials: true,
+            });
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries("user");
+            setIsEditing(false);
+        },
+    });
+
+    return (
+        <>
+            <div
+                className="modal-overlay"
+                onClick={() => {
+                    setIsEditing(false);
+                }}
+            >
+                <aside
+                    className="badge-modal"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                    }}
+                >
+                    <div>
+                        <h3>All Badges</h3>
+                        <div className="badge-select-container">
+                            {badges.map((badge) => {
+                                return (
+                                    <Badge
+                                        badge={badge}
+                                        className="user-page-badge"
+                                        key={badge.id}
+                                        onClick={() => {
+                                            toggleBadge(badge);
+                                        }}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                    <div>
+                        <h3>Shown Badges ({shown.length}/3)</h3>
+                        <div className="badge-select-container">
+                            {shown.map((badge) => {
+                                return (
+                                    <Badge
+                                        badge={badge}
+                                        className="user-page-badge"
+                                        key={badge.id}
+                                        onClick={() => {toggleBadge(badge)}}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                    <button
+                        className="edit-badge-button"
+                        onClick={() =>
+                            badgeMutation.mutate(shown.map((badge) => badge.id))
+                        }
+                    >
+                        Save
+                    </button>
+                </aside>
+            </div>
+        </>
+    );
+};
+
+export default BadgeSelector;
