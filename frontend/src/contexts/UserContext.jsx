1d44288aa397c600c380494682a851c8346cebbb
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-16 10:12:29 -0700

    [Geo] [Frontend] Add user's location to frontend

--- frontend/src/contexts/UserContext.jsx
+++ frontend/src/contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import {useLocalStorage} from "@uidotdev/usehooks"
@@ -6,6 +6,7 @@ import {useLocalStorage} from "@uidotdev/usehooks"
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
+    
     const getUserProfile = useQuery({
         queryKey: ['user', document.cookie],
         queryFn: () => {
@@ -19,6 +20,9 @@ export const UserProvider = ({ children }) => {
     });
 
     const [user, setUser] = useLocalStorage("user", null);
+
+    const MENLO_PARK_COORDS = {lng: -122.1486120978705, lat: 37.4845092388847}
+    const [userLocation, setUserLocation] = useState(MENLO_PARK_COORDS)
 
     const logOut = () => {
         const url = import.meta.env.VITE_SERVER_API;
@@ -37,9 +41,29 @@ export const UserProvider = ({ children }) => {
         }
     }, [getUserProfile.status]);
 
+        //get user's actual location
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setUserLocation({
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    });
+                },
+                (error) => {
+                    console.error('Error getting location:', error);
+                    setUserLocation(MENLO_PARK_COORDS);
+                }
+            );
+        } else {
+            setUserLocation(MENLO_PARK_COORDS);
+        }
+    }, []);
+
 
     return (
-        <UserContext.Provider value={{ user, refetch: getUserProfile.refetch, logOut }}>
+        <UserContext.Provider value={{ user, refetch: getUserProfile.refetch, logOut, location: userLocation}}>
             {children}
         </UserContext.Provider>
     );
