52d4a4edc52fe754a9b9846b5c1db8916fe3afb1
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-07-16 21:46:13 -0700

    [Fix] Loader Event Viewbox parameters

--- frontend/src/components/LoaderEvent.jsx
+++ frontend/src/components/LoaderEvent.jsx
@@ -6,7 +6,6 @@ const LoaderEvent = (props) => (
             speed={2}
             width="100%"
             height='210px'
-            viewBox="0 0 100% 100%"
             backgroundColor="#f3f3f3"
             foregroundColor="#ecebeb"
             {...props}
